6a6ccb900e60de8a75d48a17c17a187e90dcfd62
abeldinov160599 <71010650+abeldinov160599@users.noreply.github.com>
2022-10-05 18:29:38 +0600

    Add files via upload

---
+++ Python_AIS_2_LAB02_VTiP-302/main.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+import sys
+
+from PyQt5 import QtGui
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import *
+from PyQt5.uic import loadUi
+
+
+# Основной класс программы
+class Main(QDialog):
+    def __init__(self):
+        super(Main, self).__init__()
+        loadUi('frontend.ui', self)  # Загрузка формы из файла
+
+        # Задание заголовка окна
+        self.setWindowTitle('Создание простейшей визуальной '
+                            'программы на Python')
+
+
+        # Задание картинки с заданием с масштабированием в компоненте
+        self.label_img.setPixmap(QPixmap('task.png'))
+        self.label_img.setScaledContents(True)
+
+        # Привязываем к кнопкам наши процедуры-обработчики
+        self.btn_solve.clicked.connect(self.solve)
+        self.btn_clear.clicked.connect(self.clear)
+        self.btn_exit.clicked.connect(self.close)
+
+    # Процедура решения примера
+    def solve(self):
+        try:
+            a = float(self.lineEdit_a.text())
+            b = float(self.lineEdit_b.text())
+            x = float(self.lineEdit_x.text())
+            d = float(self.lineEdit_x.text())
+            try:
+                if x < 6:
+                    answer = (a + b)**2 / (x - 2)
+                else:
+                    answer = x * d**3 + b**2
+                self.label_answer.setText('Ответ: ' + str(format(answer, '.2f')))
+            except ZeroDivisionError:
+                self.label_answer.setText('Деление на ноль невозможно!')
+        except:
+            self.label_answer.setText('Неверные входные значения!')
+
+    # Процедура очистки данных
+    def clear(self):
+        self.lineEdit_a.setText('')
+        self.lineEdit_b.setText('')
+        self.lineEdit_x.setText('')
+        self.lineEdit_d.setText('')
+        self.label_answer.setText('Ответ: ')
+
+
+# Основная часть программы
+app = QApplication(sys.argv)
+window = Main()  # базовый класс окна
+window.show()  # отобразить окно на экране
+sys.exit(app.exec_())  # запуск основного цикла приложения
